8c7e30f20030537239954b19fba36f5a8afd344a
C5299055 <adam.karlowicz@sap.com>
2019-12-27 22:02:37 +0100

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8088',//'backend-dot-psomocnik-262113.appspot.com/',
+                target: 'backend-dot-psomocnik-262113.appspot.com/',
                 ws: true,
                 changeOrigin: true
             }
